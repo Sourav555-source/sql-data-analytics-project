fb5acfd1ce0b0be64ea2d47e1d6a52faf05742f8
Sourav Vilas Nikam <souravnikam555@gmail.com>
2026-02-25 23:05:31 +0530

    Document dimensions exploration SQL script
    Added comments and purpose for exploring dimension tables.

--- scripts /02_Dimensions_exploration.sql
+++ scripts /02_Dimensions_exploration.sql
@@ -1,1 +1,32 @@
 
+/*
+===============================================================================
+Dimensions Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+	
+SQL Functions Used:
+    - DISTINCT
+    - ORDER BY
+===============================================================================
+*/
+
+
+/* Identifying the unique values (or categories) in each dimension.
+	Recognizing how data might be grouped or segmented which is useful
+	for late analysis.. */
+	
+
+-- Explore All countries our customers come from
+
+select distinct country from gold.dim_customer;
+
+
+-- Explore All categories "The Major Divisions"
+
+select distinct category,
+subcategory,
+product_name
+from gold.dim_products;
+
