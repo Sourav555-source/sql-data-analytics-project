b8417d2cdd165308cdaae9c31c344fbe2b387987
Sourav Vilas Nikam <souravnikam555@gmail.com>
2026-02-25 23:02:52 +0530

    Update 01_Database_exploration.sql

--- scripts /01_Database_exploration.sql
+++ scripts /01_Database_exploration.sql
@@ -1,1 +1,23 @@
+/*
+===============================================================================
+Database Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of the database, including the list of tables and their schemas.
+    - To inspect the columns and metadata for specific tables.
+
+Table Used:
+    - INFORMATION_SCHEMA.TABLES
+    - INFORMATION_SCHEMA.COLUMNS
+===============================================================================
+*/
+
+--Explore All Object in the Database
+
+select * from INFORMATION_SCHEMA.TABLES
+
+-- Explore All Columns in the Database
+
+select * from INFORMATION_SCHEMA.COLUMNS
+where TABLE_NAME = 'dim_customer'
 
